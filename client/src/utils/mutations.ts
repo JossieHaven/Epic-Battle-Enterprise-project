d652b25ef400cfdc82a6dae1e090b088c4022bb6
mnorris3 <mrn3331324@gmail.com>
2025-03-17 13:56:16 -0400

    login now works

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,78 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($userData: UserInput!) {
+    addUser(userData: $userData) {
+      token
+      user {
+        id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_CHARACTER = gql`
+  mutation SaveCharacter(
+    $characterId: String!
+    $name: String!
+    $description: String!
+    $image: String
+    $powerstats: PowerstatsInput
+  ) {
+    saveCharacter(
+      characterId: $characterId
+      name: $name
+      description: $description
+      image: $image
+      powerstats: PowerstatsInput
+    ) {
+      CharacterCount
+      email
+      id
+      username
+      savedCharacters {
+        publisher
+        name
+        characterId
+        description
+        image
+      }
+      powerstats {
+        intelligence
+        strength
+        speed
+        durability
+        power
+        combat
+      }
+    }
+  }
+`;
+
+export const REMOVE_CHARACTER = gql`
+  mutation removeCharacter($characterId: String!) {
+    removeCharacter(characterId: $characterId) {
+      id
+      username
+      savedCharacters {
+        characterId
+        publisher
+        name
+      }
+    }
+  }
+`;
---
+++ client/src/utils/queries.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      id
+      username
+      email
+      savedCharacters {
+        characterId
+        name
+        publisher
+        description
+        image
+        powerstats
+      }
+    }
+  }
+`;
