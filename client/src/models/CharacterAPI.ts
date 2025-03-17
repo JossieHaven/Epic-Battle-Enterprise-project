8d869d66d2735688b3030346d7275cfcc423d39f
mnorris3 <mrn3331324@gmail.com>
2025-03-17 19:16:30 -0400

    update

--- client/src/models/CharacterAPI.ts
+++ client/src/models/CharacterAPI.ts
@@ -1,16 +1,22 @@
 export interface CharacterDetails {
-    name: string;
-    publisher: string;
-    description: string;
-    image: string;
-    powerstats: {intelligence: number, strength: number, speed: number, durability: number, power: number, combat: number};
-    allignment: string;
-    gender: string;
-    race: string
-  }
-  
-  export interface ICharacter {
-      id: string;
-      stats: CharacterDetails;
-  }
-  
+  name: string;
+  publisher: string;
+  description: string;
+  image: string;
+  powerstats: {
+    intelligence: number;
+    strength: number;
+    speed: number;
+    durability: number;
+    power: number;
+    combat: number;
+  };
+  allignment: string;
+  gender: string;
+  race: string;
+}
+
+export interface ICharacter {
+  id: string;
+  stats: CharacterDetails;
+}
