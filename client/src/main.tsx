8d7b0b5cc07c6acf52fadfac6d3d8c7841568091
jgmaxen <maxen.jonathan@gmail.com>
2025-03-18 19:19:50 -0400

    debugged

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,33 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './App.css'; 
-import App from './App';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import SearchCharacters from './pages/SearchCharacters';
-import BattleArena from './pages/BattleArena'; 
-import ErrorPage from './pages/Error';
-import Profile from './pages/UserProfile';
+import "./App.css";
+import App from "./App";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import SearchCharacters from "./pages/SearchCharacters";
+import BattleArena from "./pages/BattleArena";
+import ErrorPage from "./pages/Error";
+import UserProfile from "./pages/UserProfile";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/*",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
-      { path: "/login", element: <Login /> },
-      { path: "/signup", element: <Signup /> },
-      { path: "/search", element: <SearchCharacters /> },
-      { path: "/battle", element: <BattleArena /> },
-      { path: "/profile", element: <Profile /> } 
-    ]
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
+      { path: "search", element: <SearchCharacters /> },
+      { path: "battle", element: <BattleArena /> },
+      { path: "profile", element: <UserProfile /> },
+    ],
   },
 ]);
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <RouterProvider router={router} />
