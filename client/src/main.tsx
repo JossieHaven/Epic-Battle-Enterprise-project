5f4afb61ca391595c249a9fb0e52c940fbcb6154
mnorris3 <mrn3331324@gmail.com>
2025-03-18 14:37:59 -0400

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './App.css'; 
-import App from './App';
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import ErrorPage from './pages/Error';
-import SearchCharacters from './pages/SearchCharacters';
+import "./App.css";
+import App from "./App";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ErrorPage from "./pages/Error";
+import SearchCharacters from "./pages/SearchCharacters";
 
 const router = createBrowserRouter([
   {
@@ -19,8 +19,8 @@ const router = createBrowserRouter([
       { path: "/login", element: <Login /> },
       { path: "/signup", element: <Signup /> },
       { path: "/character-selection", element: <CharacterSelection /> },
-      { path: "/battle", element: <BattleArena /> }
-    ]
+      { path: "/battle", element: <BattleArena /> },
+    ],
   },
 ]);
 
